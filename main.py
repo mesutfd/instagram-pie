38e36bc31b2b90cbd287ba2c1406604f1a3351e4
a.rahmani <a.rahmani@git.fn>
2022-08-20 14:07:27 +0430

    test 20

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ from routers import (
     insights, send_direct
 )
 
-app = FastAPI()
-app.include_router(auth.router,prefix='/instagram/engine/instagrapi')
-app.include_router(media.router,prefix='/instagram/engine/instagrapi')
-app.include_router(video.router,prefix='/instagram/engine/instagrapi')
-app.include_router(photo.router,prefix='/instagram/engine/instagrapi')
-app.include_router(user.router,prefix='/instagram/engine/instagrapi')
-app.include_router(igtv.router,prefix='/instagram/engine/instagrapi')
-app.include_router(clip.router,prefix='/instagram/engine/instagrapi')
-app.include_router(album.router,prefix='/instagram/engine/instagrapi')
-app.include_router(story.router,prefix='/instagram/engine/instagrapi')
-app.include_router(insights.router,prefix='/instagram/engine/instagrapi')
-app.include_router(send_direct.router,prefix='/instagram/engine/instagrapi')
+app = FastAPI(prefix = '/instagram/engine/instagrapi')
+app.include_router(auth.router)
+app.include_router(media.router)
+app.include_router(video.router)
+app.include_router(photo.router)
+app.include_router(user.router)
+app.include_router(igtv.router)
+app.include_router(clip.router)
+app.include_router(album.router)
+app.include_router(story.router)
+app.include_router(insights.router)
+app.include_router(send_direct.router)
 
 
-@app.get("/instagram/engine/instagrapi/", tags=["system"], summary="Redirect to /instagram/engine/instagrapi/docs")
+@app.get("/", tags=["system"], summary="Redirect to /instagram/engine/instagrapi/docs")
 async def root(request: Request):
     """Redirect to /instagram/engine/instagrapi/docs
     """
@@ -35,7 +35,7 @@ async def root(request: Request):
     )
 
 
-@app.get("/instagram/engine/instagrapi/version", tags=["system"], summary="Get dependency versions")
+@app.get("/version", tags=["system"], summary="Get dependency versions")
 async def version():
     """Get dependency versions
     """
